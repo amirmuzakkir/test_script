deb139c7e44cc0a0a1d378d380dfc14aff791ce9
amirmuzakkir <128115278+amirmuzakkir@users.noreply.github.com>
2023-07-28 15:02:58 +0800

    changes on key_name

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 resource "aws_instance" "Demo" {
   ami           = "ami-0df7a207adb9748c7"
   instance_type = "t2.micro"
-  key_name = "key_name"
+  key_name = "ec2keypair"
   tags = {
     Name = "Terraform_DemoEC2"
   }
